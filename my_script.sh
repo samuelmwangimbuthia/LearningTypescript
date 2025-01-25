4565b5e3f14cd88b7d44237e39c053dd6b8b5ec5
Samuel <mbuthia.jsamuel@gmail.com>
2025-01-25 05:35:45 +0300

    SHELL

--- my_script.sh
+++ my_script.sh
@@ -67,3 +67,77 @@ echo $a
 #using (()) for arithmetic operations
 ((a++))
 echo $a
+# Conditional Statements in shell scripting
+# if statements:
+let b=51
+if [ $a -gt $b ]; then
+	echo "a is greater than b"
+elif [ $a -eq $b ]; then # always put spaces around operators and operands in conditions
+	echo "a equals b"
+else
+	echo "a is less than b"
+fi
+
+#Case Statement
+case $a in
+	a) echo "a is 1" ;; 
+	b) echo "a is 2" ;;
+	*) echo "a is neither 1 nor 2" ;;
+esac
+<<COMMENT
+Loops are powerful tools for automating repetitive tasks.you can use them for various
+purposes like 
+- iterating over files, arrays or ranges
+- you can rename all files in a directory using a loop
+COMMENT
+# for loop
+for i in {1..3}; do
+	echo "Number: $i"
+done
+
+# while loop
+count=1
+while [ $count -le 3 ]; do
+	echo "count is $count"
+	((count++))
+done
+
+# until loop- continues to execute until the condition becomes true
+count2 = 1
+until [ $count2 -gt 3 ]; do
+	echo "Count2 is: $count2"
+	((count2++))
+done
+
+# infinite loop
+# continues running indefinitely until its manually stopped
+#for (( ; ; )); do
+#	echo "This is infinite loop"
+#done
+
+#Select Loop
+#creates a simple menu system which allows users to select an option from a list.its useful when you need a user driven selection process.
+# the select loop displays options and each selection runs the corresponding case statement.
+PS3="Choose a fruit: "
+select option in Apple Banana Orange Mango Exit; do
+	case $option in
+		Apple) 
+			echo "You chose Apple";;
+		Banana)
+			echo "You chose Banana";;
+		Orange)
+			echo "You chose Orange";;
+		Exit) break;; *) 
+			echo "Invalid option";;
+
+	esac
+done	
+
+
+#Functions
+#Useful for encapsulating a reusable block of code
+greet_user() {
+	echo "hello, $1!"
+}
+
+greet_user "Samuel"
